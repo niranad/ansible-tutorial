ac7be0cd4bde5cb246d9fbdd060f6062a81d696f
Adeniran J. Olukanni <adesite67@gmail.com>
2022-10-06 17:40:05 +0100

    Change server console message in roles/setup/files/index.js

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Hello World');
+  res.end("Hello World!!! So excited to be using WSL!");
 });
 server.listen(3000);
